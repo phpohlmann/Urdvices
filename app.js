be977f35deb6b4c777858913429bd5a6f9430a61
Pedro Pohlmann <123262633+phpohlmann@users.noreply.github.com>
2023-10-02 04:56:00 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const consDiv = document.querySelector('#conselhoDiv');
+const contDiv = document.querySelector('#contadorDiv');
+const resBtn = document.querySelector('#getCons');
+
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+
+
+function toggleOpacity() {
+  header.classList.toggle('opacidadeZero');
+  main.classList.toggle('opacidadeZero');
+  footer.classList.toggle('opacidadeZero');
+}
+
+function getAdvice(){
+  fetch('https://api.adviceslip.com/advice').then(response => {
+    return response.json();
+  }).then(adviceData => {
+    contDiv.innerHTML = `<p>#${adviceData.slip.id}</p>`;
+    const AdviceObj = adviceData.slip;
+    consDiv.innerHTML = `<p>${AdviceObj.advice}</p>`;
+  }).catch(error => {
+    console.log(error);
+  })
+}
+
+resBtn.addEventListener('click', () => {
+  toggleOpacity();
+  setTimeout(() => {
+    toggleOpacity();
+  }, 1000);
+ getAdvice()
+});
